020260ca06537edbda2233a8f420f84aad5b6272
Kilian Domaratius <github@kdomaratius.de>
2021-12-19 17:53:59 +0100

    add link to donations

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,12 @@
 <script context="module">
 	import { _ } from '$lib/i18n';
-	import Title from '$lib/header/Title.svelte'
-
+	import Title from '$lib/header/Title.svelte';
+	import DonationCard from '$lib/donations/card.svelte';
 
 	export const prerender = true;
 </script>
 
-<Title title="{$_('home.title')}"></Title>
+<Title title={$_('home.title')} />
 
 <div class="content">
 	<h1>{$_('home.title')}</h1>
@@ -25,16 +25,23 @@
 			Daraus ist diese Seite entstanden. Hier könnt ihr sehen für welchen guten Zweck wir uns für
 			die einzelnen Teile in der Familie entschieden haben.
 		</p>
+		>&nbsp;<a href="/donations">Alle Spenden anzeigen</a>
 	{:else}
 		<p>Merry Christmas from Verena & Kilian</p>
 		<p>
-			This year everything will be different again at Christmas. We have taken this as an opportunity this year not only to bring joy to those who receive gifts, but also to do more good.
+			This year everything will be different again at Christmas. We have taken this as an
+			opportunity this year not only to bring joy to those who receive gifts, but also to do more
+			good.
 		</p>
 		<p>
-			Therefore, we sat down and spent a long time thinking about what we could do for each part of the family. Our goal was to find projects, organizations and associations that would also suit you or that you would like to support yourselves.
+			Therefore, we sat down and spent a long time thinking about what we could do for each part of
+			the family. Our goal was to find projects, organizations and associations that would also suit
+			you or that you would like to support yourselves.
 		</p>
 		<p>
-			The result is this page. Now you can see for which charity we have decided for the for the various parts in the family.
+			The result is this page. Now you can see for which charity we have decided for the for the
+			various parts in the family.
 		</p>
+		>&nbsp;<a href="/donations">See all donations</a>
 	{/if}
 </div>
