98c3a0fdb6d00f0c4f7202125ff640a8c54da0bf
Kilian Domaratius <github@kdomaratius.de>
2024-01-23 19:44:58 +0000

    svelte-migrate: updated files

--- src/routes/imprint/+page.svelte
+++ src/routes/imprint/+page.svelte
@@ -1,20 +1,6 @@
 <script context="module">
 	import { _ } from '$lib/i18n';
 	import Title from '$lib/header/Title.svelte'
-
-	import { browser, dev } from '$app/env';
-
-	// we don't need any JS on this page, though we'll load
-	// it in dev so that we get hot module replacement...
-	export const hydrate = dev;
-
-	// ...but if the client-side router is already loaded
-	// (i.e. we came here from elsewhere in the app), use it
-	export const router = browser;
-
-	// since there's no dynamic data here, we can prerender
-	// it so that it gets served as a static asset in prod
-	export const prerender = true;
 </script>
 
 <Title title="{$_('imprint.title')}"></Title>
