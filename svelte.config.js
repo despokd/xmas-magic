b9236a60ba730c02b6aed85ffcd0e800d06c066b
despokd <github@kdomaratius.de>
2024-01-23 23:43:17 +0100

    fix: migrate to svelte4

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,13 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
 
 export default {
-	plugins: [ sveltekit() ]
-  };
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter()
+  },
+  preprocess: [preprocess({}), vitePreprocess({})]
+};
